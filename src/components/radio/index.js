1b8de9c4c3bdba3dcd2d05fdd23bc2384f003c7b
Alex Krolick <akrolick@box.com>
2019-05-01 11:31:27 -0700

    build(examples): v10.2.0-beta.6

--- src/components/radio/index.js
+++
@@ -1,4 +0,0 @@
-// @flow
-export { default as RadioButton } from './RadioButton';
-export { default as RadioGroup } from './RadioGroup';
-export { default as RadioGroupField } from './RadioGroupField';
