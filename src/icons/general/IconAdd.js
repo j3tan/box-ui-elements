b654246867039ab5db329dbc2ff966b11c2ed44c
Jeff Tan <jtan@box.com>
2019-07-08 17:46:33 -0700

    chore: test

--- src/icons/general/IconAdd.js
+++ src/icons/general/IconAdd.js
@@ -4,6 +4,8 @@ import * as React from 'react';
 import AccessibleSVG from '../accessible-svg';
 import { bdlGray } from '../../styles/variables';
 
+// tests
+
 type Props = {
     className?: string,
     color?: string,
